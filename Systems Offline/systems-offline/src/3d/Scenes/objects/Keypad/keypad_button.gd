b183251375771310d66c8ba388c8f7eae54e2502
Teun <w.t.klaassens@student.tudelft.nl>
2026-09-14 21:48:43 +0200

    Add LockLED and visual feedback to keypad

--- Systems Offline/systems-offline/src/3d/Scenes/objects/Keypad/keypad_button.gd
+++ Systems Offline/systems-offline/src/3d/Scenes/objects/Keypad/keypad_button.gd
@@ -3,5 +3,5 @@ extends StaticBody3D
 @export var key_value: String
 
 func interact() -> void:
-	print("Hit node: ", name, " | key value: ", key_value)
+	#print("Hit node: ", name, " | key value: ", key_value)
 	get_parent().press_key(key_value)
--- Systems Offline/systems-offline/src/3d/Scenes/objects/keypad.gd
+++ Systems Offline/systems-offline/src/3d/Scenes/objects/keypad.gd
@@ -2,12 +2,14 @@ extends Node3D
 
 @onready var screen_label: Label3D = $Screen
 @export var sliding_door: StaticBody3D
+@onready var lock_led: MeshInstance3D = $LockLED
 
 var entered_code := ""
 var correct_code := "51515"
 
 func _ready() -> void:
 	update_screen()
+	update_led(true)
 
 func press_key(key: String) -> void:
 	if key == "C":
@@ -29,8 +31,19 @@ func check_code() -> void:
 	if entered_code == correct_code:
 		print("Correct code!")
 		sliding_door.unlock()
+		update_led(false)
 	else:
 		print("Wrong code!")
 		entered_code = ""
 
 	update_screen()
+	
+func update_led(locked: bool) -> void:
+	var material := lock_led.get_active_material(0) as StandardMaterial3D
+
+	if locked:
+		material.albedo_color = Color.RED
+		material.emission = Color.RED
+	else:
+		material.albedo_color = Color.GREEN
+		material.emission = Color.GREEN
